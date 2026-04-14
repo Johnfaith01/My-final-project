e94cff3cf1880341e4099aa730228f9d74451d92
Johnfaith01 <bamisayejohnfaith@gmail.com>
2026-04-14 08:55:27 +0100

    my first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App(){
+
+  return(
+    <>
+      <div className="bg-red-500">
+          <h1 className="text-2xl text-white">Welcome to the Hotel Management App</h1>
+      </div>
+      
+    </>
+  )
+
+}
+
+export default App
